03e98fb49a5e0ac90defaa22a430e27ff14071f7
RodrigoGabriel <monntedeoca@hotmail.com>
2020-03-03 14:55:03 -0300

    Add files via upload

---
+++ src/app/App.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+//import {TextField,Button} from '@material-ui/core'
+
+const Component = () => {
+
+  //  const [city, setCity] = useState("Buenos Aires")
+   /*
+    var today = new Date();
+    var fecha = today.getDate()+"-"+(today.getMonth() +1)+ "-" + today.getFullYear()
+    var hra = today.getHours() + ":" + today.getMinutes();*/
+
+  
+
+ 
+    
+    //console.log(weather)
+
+    return <div >
+        
+       <h1>react app</h1> 
+
+    </div>
+}
+
+export default Component;
---
+++ src/app/index.js
@@ -0,0 +1,18 @@
+import "./index.css";
+import React from 'react'
+import { render } from 'react-dom'
+import Component from './App.js'
+
+console.log("works")
+
+
+
+render(
+   <Component/>,
+   document.getElementById('reactApp')
+)
+
+
+
+
+//Pipo_lucidoZ
